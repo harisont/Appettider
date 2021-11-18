06e368e37b4648ca8bb97eaeb43b17bdbc3a9b43
Arianna Masciolini <uzkamascio@gmail.com>
2021-11-18 22:57:57 +0100

    define Place entity

--- android/app/src/main/java/bho/harisont/appettider/db/Place.kt
+++ android/app/src/main/java/bho/harisont/appettider/db/Place.kt
@@ -1,4 +1,17 @@
 package bho.harisont.appettider.db
 
-class Place {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+enum class Weekday {
+    MON, TUE, WED, THU, FRI, SAT, SUN
 }
+
+@Entity
+data class Place (
+        @PrimaryKey val name: String,
+        @ColumnInfo val address: String,
+        @ColumnInfo val openingTimes: Map<Weekday, String>,
+        @ColumnInfo val notes: String
+)
